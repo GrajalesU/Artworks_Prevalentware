542a38aaa442ca0c81529a85c64829f5642e51ec
Juan Manuel Grajales <juan.grajalesu@gmail.com>
2023-04-15 15:28:00 -0500

    Add artwork post

---
+++ src/pages/api/artwork/index.ts
@@ -0,0 +1,33 @@
+import { PrismaClient } from "@prisma/client";
+import { NextApiHandler } from "next";
+
+const prisma = new PrismaClient();
+
+const artwork: NextApiHandler<Artwork> = async (request, response) => {
+  if (request.method !== "POST") {
+    response.status(405).end();
+    return;
+  }
+
+  const currentArtworks = await prisma.artwork.findMany();
+  const sameArtwork = currentArtworks.find(
+    (artwork) => artwork.title === request.body.title
+  );
+
+  if (sameArtwork) {
+    response.status(200).json(sameArtwork);
+    return;
+  }
+
+  const newArtwork = await prisma.artwork.create({
+    data: {
+      title: request.body.title,
+      artist: request.body.artist,
+      image_url: request.body.image_url,
+    },
+  });
+
+  response.status(201).json(newArtwork);
+};
+
+export default artwork;
