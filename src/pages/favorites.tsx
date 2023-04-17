644d68a7d79f4b4ee7e970dd4c62b52020d1d6bd
Juan Manuel Grajales <juan.grajalesu@gmail.com>
2023-04-17 00:55:17 -0500

    Add user info

--- src/pages/favorites.tsx
+++ src/pages/favorites.tsx
@@ -10,6 +10,14 @@ export default function Favorites() {
   return (
     <PrivateRoute>
       <PrincipalLayout>
+        <div className="m-10">
+          <h1 className="text-3xl font-bold">Favorites</h1>
+          <h2 className="text-xl font-semibold">
+            Hi, {user?.name}{" "}
+            <span className="text-gray-400">({user?.email})</span>
+          </h2>
+          <p className="">You have {user?.favorites.length} favorites</p>
+        </div>
         <section className="my-10 min-h-[85vh] px-4 container mx-auto">
           <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
             {user?.favorites.map(({ artwork }) => (
