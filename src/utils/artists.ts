ad712d858af42b6ce19bf9e44de311f5c223ea68
Juan Manuel Grajales <juan.grajalesu@gmail.com>
2023-04-16 23:17:22 -0500

    Add artists

---
+++ src/utils/artists.ts
@@ -0,0 +1,403 @@
+/* cSpell:disable */
+export const ARTISTS = [
+  {
+    value: "",
+    label: "All artists",
+  },
+  {
+    value: "anoniem",
+    label: "anoniem",
+  },
+  {
+    value: "onbekend",
+    label: "onbekend",
+  },
+  {
+    value: "Ed van der Elsken",
+    label: "Ed van der Elsken",
+  },
+  {
+    value: "George Hendrik Breitner",
+    label: "George Hendrik Breitner",
+  },
+  {
+    value: "Jan Luyken",
+    label: "Jan Luyken",
+  },
+  {
+    value: "Reinier Vinkeles (I)",
+    label: "Reinier Vinkeles (I)",
+  },
+  {
+    value: "Rembrandt van Rijn",
+    label: "Rembrandt van Rijn",
+  },
+  {
+    value: "Daniel Nikolaus Chodowiecki",
+    label: "Daniel Nikolaus Chodowiecki",
+  },
+  {
+    value: "Bernard Picart",
+    label: "Bernard Picart",
+  },
+  {
+    value: "Claes Jansz. Visscher (II)",
+    label: "Claes Jansz. Visscher (II)",
+  },
+  {
+    value: "Lodewijk XIV (koning van Frankrijk)",
+    label: "Lodewijk XIV (koning van Frankrijk)",
+  },
+  {
+    value: "Printshop",
+    label: "Printshop",
+  },
+  {
+    value: "Hendrick Goltzius",
+    label: "Hendrick Goltzius",
+  },
+  {
+    value: "Marius Bauer",
+    label: "Marius Bauer",
+  },
+  {
+    value: "Franse kroon",
+    label: "Franse kroon",
+  },
+  {
+    value: "Philips Galle",
+    label: "Philips Galle",
+  },
+  {
+    value: "Antonio Tempesta",
+    label: "Antonio Tempesta",
+  },
+  {
+    value: "Isaac Israels",
+    label: "Isaac Israels",
+  },
+  {
+    value: "Johannes Tavenraat",
+    label: "Johannes Tavenraat",
+  },
+  {
+    value: "Peter Paul Rubens",
+    label: "Peter Paul Rubens",
+  },
+  {
+    value: "Willem Witsen",
+    label: "Willem Witsen",
+  },
+  {
+    value: "Pieter Schenk (I)",
+    label: "Pieter Schenk (I)",
+  },
+  {
+    value: "Rafaël",
+    label: "Rafaël",
+  },
+  {
+    value: "Jacobus Buys",
+    label: "Jacobus Buys",
+  },
+  {
+    value: "Crispijn van de Passe (I)",
+    label: "Crispijn van de Passe (I)",
+  },
+  {
+    value: "Romeyn de Hooghe",
+    label: "Romeyn de Hooghe",
+  },
+  {
+    value: "Aat Veldhoen",
+    label: "Aat Veldhoen",
+  },
+  {
+    value: "Jacques de Gheyn (II)",
+    label: "Jacques de Gheyn (II)",
+  },
+  {
+    value: "Vincent Samuel Mentzel",
+    label: "Vincent Samuel Mentzel",
+  },
+  {
+    value: "Maerten de Vos",
+    label: "Maerten de Vos",
+  },
+  {
+    value: "Willem Diepraam",
+    label: "Willem Diepraam",
+  },
+  {
+    value: "Simon Fokke",
+    label: "Simon Fokke",
+  },
+  {
+    value: "Staten van Holland en West-Friesland",
+    label: "Staten van Holland en West-Friesland",
+  },
+  {
+    value: "Giovanni Giacomo de'Rossi",
+    label: "Giovanni Giacomo de'Rossi",
+  },
+  {
+    value: "Anthony van Dyck",
+    label: "Anthony van Dyck",
+  },
+  {
+    value: "anoniem (Monumentenzorg)",
+    label: "anoniem (Monumentenzorg)",
+  },
+  {
+    value: "Johann Sadeler (I)",
+    label: "Johann Sadeler (I)",
+  },
+  {
+    value: "Goupil & Cie.",
+    label: "Goupil & Cie.",
+  },
+  {
+    value: "Jacob Houbraken",
+    label: "Jacob Houbraken",
+  },
+  {
+    value: "Adriaen Collaert",
+    label: "Adriaen Collaert",
+  },
+  {
+    value: "Aubert & Cie.",
+    label: "Aubert & Cie.",
+  },
+  {
+    value: "Jacques Callot",
+    label: "Jacques Callot",
+  },
+  {
+    value: "Carel Adolph Lion Cachet",
+    label: "Carel Adolph Lion Cachet",
+  },
+  {
+    value: "Johan Braakensiek",
+    label: "Johan Braakensiek",
+  },
+  {
+    value: "Frans Hogenberg",
+    label: "Frans Hogenberg",
+  },
+  {
+    value: "Eva Charlotte Pennink-Boelen",
+    label: "Eva Charlotte Pennink-Boelen",
+  },
+  {
+    value: "Sébastien Leclerc (I)",
+    label: "Sébastien Leclerc (I)",
+  },
+  {
+    value: "Hieronymus Wierix",
+    label: "Hieronymus Wierix",
+  },
+  {
+    value: "Jean Lepautre",
+    label: "Jean Lepautre",
+  },
+  {
+    value: "Gerard de Jode",
+    label: "Gerard de Jode",
+  },
+  {
+    value: "Joseph Maes",
+    label: "Joseph Maes",
+  },
+  {
+    value: "Joseph Rose Lemercier",
+    label: "Joseph Rose Lemercier",
+  },
+  {
+    value: "Anna Beeck",
+    label: "Anna Beeck",
+  },
+  {
+    value: "diverse vervaardigers",
+    label: "diverse vervaardigers",
+  },
+  {
+    value: "Stefano della Bella",
+    label: "Stefano della Bella",
+  },
+  {
+    value: "Wenceslaus Hollar",
+    label: "Wenceslaus Hollar",
+  },
+  {
+    value: "Jan van de Velde (II)",
+    label: "Jan van de Velde (II)",
+  },
+  {
+    value: "Pieter van der Aa (I)",
+    label: "Pieter van der Aa (I)",
+  },
+  {
+    value: "Abraham Bloemaert",
+    label: "Abraham Bloemaert",
+  },
+  {
+    value: "Hieronymus Cock",
+    label: "Hieronymus Cock",
+  },
+  {
+    value: "Giovanni Battista Piranesi",
+    label: "Giovanni Battista Piranesi",
+  },
+  {
+    value: "Maarten van Heemskerck",
+    label: "Maarten van Heemskerck",
+  },
+  {
+    value: "Richard Nicolaüs Roland Holst",
+    label: "Richard Nicolaüs Roland Holst",
+  },
+  {
+    value: "Jozef Israëls",
+    label: "Jozef Israëls",
+  },
+  {
+    value: "veuve Delpech (Naudet)",
+    label: "veuve Delpech (Naudet)",
+  },
+  {
+    value: "Crispijn van de Passe (II)",
+    label: "Crispijn van de Passe (II)",
+  },
+  {
+    value: "Jan Caspar Philips",
+    label: "Jan Caspar Philips",
+  },
+  {
+    value: "Pieter Mortier (I)",
+    label: "Pieter Mortier (I)",
+  },
+  {
+    value: "Jan Veth",
+    label: "Jan Veth",
+  },
+  {
+    value: "Emrik & Binger",
+    label: "Emrik & Binger",
+  },
+  {
+    value: "Meissener Porzellan Manufaktur",
+    label: "Meissener Porzellan Manufaktur",
+  },
+  {
+    value: "v/h Ellerman, Harms & Co. Amsterdamsche Boek- en Steendrukkerij",
+    label: "v/h Ellerman, Harms & Co. Amsterdamsche Boek- en Steendrukkerij",
+  },
+  {
+    value: "Aegvalueius Sadeler (II)",
+    label: "Aegvalueius Sadeler (II)",
+  },
+  {
+    value: "Pauselijk hof",
+    label: "Pauselijk hof",
+  },
+  {
+    value: "Adriaen van Ostade",
+    label: "Adriaen van Ostade",
+  },
+  {
+    value: "Hendrick Hondius (I)",
+    label: "Hendrick Hondius (I)",
+  },
+  {
+    value: "Martinus Nijhoff",
+    label: "Martinus Nijhoff",
+  },
+  {
+    value: "Johan Michaël Schmvaluet Crans",
+    label: "Johan Michaël Schmvaluet Crans",
+  },
+  {
+    value: "Hendrik Spilman",
+    label: "Hendrik Spilman",
+  },
+  {
+    value: "Boëtius Adamsz. Bolswert",
+    label: "Boëtius Adamsz. Bolswert",
+  },
+  {
+    value: "Jan Philipsz Schabaelje",
+    label: "Jan Philipsz Schabaelje",
+  },
+  {
+    value: "Caspar Luyken",
+    label: "Caspar Luyken",
+  },
+  {
+    value: "Gerrit Willem Dijsselhof",
+    label: "Gerrit Willem Dijsselhof",
+  },
+  {
+    value: "Jeanne Bieruma Oosting",
+    label: "Jeanne Bieruma Oosting",
+  },
+  {
+    value: "Claude Mellan",
+    label: "Claude Mellan",
+  },
+  {
+    value: "Reinier Willem Petrus de Vries (1874-1952)",
+    label: "Reinier Willem Petrus de Vries (1874-1952)",
+  },
+  {
+    value: "Theodoor Galle",
+    label: "Theodoor Galle",
+  },
+  {
+    value: "Cornelis Pronk",
+    label: "Cornelis Pronk",
+  },
+  {
+    value: "Theo van Hoytema",
+    label: "Theo van Hoytema",
+  },
+  {
+    value: "Israël Silvestre",
+    label: "Israël Silvestre",
+  },
+  {
+    value: "Leo Gestel",
+    label: "Leo Gestel",
+  },
+  {
+    value: "Charles Le Brun",
+    label: "Charles Le Brun",
+  },
+  {
+    value: "Georg Rueter",
+    label: "Georg Rueter",
+  },
+  {
+    value: "Frederik de Wit",
+    label: "Frederik de Wit",
+  },
+  {
+    value: "Albrecht Dürer",
+    label: "Albrecht Dürer",
+  },
+  {
+    value: "Anton Mauve",
+    label: "Anton Mauve",
+  },
+  {
+    value: "Virgilius Solis",
+    label: "Virgilius Solis",
+  },
+  {
+    value: "Israël Henriet",
+    label: "Israël Henriet",
+  },
+  {
+    value: "Frans Buffa en Zonen",
+    label: "Frans Buffa en Zonen",
+  },
+];
