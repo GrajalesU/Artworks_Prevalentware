b3a50ea1fa2478a97799edbd5b659118bae7fd22
Juan Manuel Grajales <juan.grajalesu@gmail.com>
2023-04-17 00:24:23 -0500

    Add favorites page

--- src/pages/favorites.tsx
+++ src/pages/favorites.tsx
@@ -1,14 +1,22 @@
+import ArtworkCard from "@/components/Artwork/ArtworkCard";
 import PrivateRoute from "@/components/PrivateRoute";
+import { useUserContext } from "@/context/User";
 import PrincipalLayout from "@/layout/Principal";
 import React from "react";
 
-export default function favorites() {
+export default function Favorites() {
+  const { user } = useUserContext();
+
   return (
     <PrivateRoute>
       <PrincipalLayout>
-        <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-          Favorites
-        </h1>
+        <section className="my-10 min-h-[85vh] px-4 container mx-auto">
+          <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+            {user?.favorites.map(({ artwork }) => (
+              <ArtworkCard key={artwork.id} artwork={artwork} />
+            ))}
+          </div>
+        </section>
       </PrincipalLayout>
     </PrivateRoute>
   );
