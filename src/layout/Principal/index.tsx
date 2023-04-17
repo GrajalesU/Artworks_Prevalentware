3efa5717301a73d0d11e550eea1bcd33cbabf839
Juan Manuel Grajales <juan.grajalesu@gmail.com>
2023-04-16 23:17:12 -0500

    Add functional search

--- src/layout/Principal/index.tsx
+++ src/layout/Principal/index.tsx
@@ -1,5 +1,4 @@
 import Header from "@/components/Header";
-import Search from "@/components/Search";
 import { useRouter } from "next/router";
 import React from "react";
 
@@ -12,11 +11,6 @@ export default function PrincipalLayout({ children }: PrincipalLayoutProps) {
   return (
     <>
       <Header currentPage={pathname} />
-      <div className=" bg-white border-t border-gray-300 px-4 lg:px-6 py-2.5 dark:bg-gray-800">
-        <div className="max-w-screen-xl mx-auto">
-          <Search />
-        </div>
-      </div>
       {children}
     </>
   );
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,14 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import ArtworkList from "@/components/Artwork/ArtworkList";
 import PrivateRoute from "@/components/PrivateRoute";
 import PrincipalLayout from "@/layout/Principal";
 import { initFlowbite } from "flowbite";
+import Search from "@/components/Search";
 
 const Home = () => {
+  const [artist, setArtist] = useState("");
+  const [query, setQuery] = useState("");
+
   useEffect(() => {
     initFlowbite();
   }, []);
@@ -12,8 +16,13 @@ const Home = () => {
   return (
     <PrivateRoute>
       <PrincipalLayout>
+        <div className=" bg-white border-t border-gray-300 px-4 lg:px-6 py-2.5 dark:bg-gray-800">
+          <div className="max-w-screen-xl mx-auto">
+            <Search setArtist={setArtist} setQuery={setQuery} />
+          </div>
+        </div>
         <div className="mx-auto">
-          <ArtworkList />
+          <ArtworkList artist={artist} q={query} />
         </div>
       </PrincipalLayout>
     </PrivateRoute>
